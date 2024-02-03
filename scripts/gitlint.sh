d2e5602d20689d39e79bd35c4fbb897a6b655547
Cla <clamattia@gmail.com>
2024-02-03 19:25:12 +0100

    gitlint action summary
    Add a summary to the GITHUB_STEP_SUMMARY. To achive this add a new
    helper script to format the stdout and stderr of a script into an
    informative message to be forwarded to GITHUB_OUT.

--- scripts/gitlint.sh
+++ scripts/gitlint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
+gitlint --version
+printf "Documentation: https://jorisroovers.com/gitlint/latest/\n\n"
 gitlint --commits "$BRANCH"
 
