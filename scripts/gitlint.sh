2866296af44041385be625c0c059c66f95510a09
Cla <clamattia@gmail.com>
2024-02-04 15:26:44 +0100

    Test for shellckeck
    Introduce a small problem to test shellckeck

--- scripts/gitlint.sh
+++ scripts/gitlint.sh
@@ -3,5 +3,5 @@ set -e
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 gitlint --version
 printf "Documentation: https://jorisroovers.com/gitlint/latest/\n\n"
-gitlint --commits "$BRANCH"
+gitlint --commits $BRANCH
 
