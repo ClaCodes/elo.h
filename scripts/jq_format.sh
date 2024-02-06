22cd4a554f269aa0bc8e455ea84bc84c00c6bade
Cla <clamattia@gmail.com>
2024-02-06 08:10:35 +0100

    github action jq_format
    Add a github action to verify formating of json files.
    Add a script to quickly fix formatting problems.

---
+++ scripts/jq_format.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+EXIT_CODE=0
+FILES=$(find . -name "*.json")
+for FILE in $FILES; do
+    if jq . "$FILE" >"$FILE.temp" 2> "$FILE.err";
+    then
+        if ! diff --unified "$FILE" "$FILE.temp" > "$FILE.diff"; then
+            EXIT_CODE=1
+        fi
+        if [ "$1" == "--diff" ]; then
+             cat "$FILE.diff"
+        elif [ "$1" == "--apply" ]; then
+            cp "$FILE.temp" "$FILE"
+        else
+            printf "%s;; \`jq\` would format this file. Use \`./scripts/jq_format.sh --diff\` to see the changes it would make and \`./scripts/jq_format.sh --apply\` to aply them.\n" "$FILE"
+        fi
+        rm "$FILE.diff"
+    fi
+    rm "$FILE.temp"
+    rm "$FILE.err"
+done
+
+exit "$EXIT_CODE"
