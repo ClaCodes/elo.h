65498e13b5a2ffc2b39f0a2c4ff0b744b690453f
Cla <clamattia@gmail.com>
2024-02-11 14:52:00 +0100

    Test for shfmt
    Introduce a small formatting problem in shell scripts to test shfmt.

--- scripts/jq_valid.sh
+++ scripts/jq_valid.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 EXIT_CODE=0
 FILES=$(find . -name "*.json")
-for FILE in $FILES; do
-    if ! jq . "$FILE" >"$FILE.temp" 2>"$FILE.err"; then
+for FILE in $FILES;
+do
+    if ! jq . "$FILE" >"$FILE.temp" 2>"$FILE.err";
+    then
         EXIT_CODE=1
         printf "%s;;%s\n" "$FILE" "$(<"$FILE.err")"
     fi
